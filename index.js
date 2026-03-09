a77a9f43955c71fa2433964c49854d5eaf3fff53
AcronyxElectronator <acronyxelectronator@gmail.com>
2026-03-09 18:17:45 -0400

    Task 5.1 arrays

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+/*let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
 
 for(let i=0; i< arr.length; i++){
   console.log(arr[i])
 }
+*/
+
 /*task 2
 let radius = 7;
 const pi= 3.14;
@@ -73,10 +75,35 @@ add(5, 10, happyPrint);
 add(11, 12, sadPrint);
 */
 
-//Task 2 (set interval and timeout)
+/*Task 2 (set interval and timeout)
 //prints current date
 function printDate(){
    console.log(Date().toLocaleTimeString());
 }
       
 setInterval(printDate, 1000);
+*/
+
+//task 5.1 arrays
+let arr = [ -5, 16, 33, 42, 103, 344];
+
+console.log(arr.includes(-5));//true
+
+arr.push(11);//adds item to the end
+
+console.log(arr)//[ -5, 16, 33, 42, 103, 344, 11];
+
+let lastItem = arr.pop();//removes last item
+
+console.log(lastItem, arr);//11, [ -5, 16, 33, 42, 103, 344]
+
+arr.unshift(22);//adds item to the front
+
+console.log(arr);//[22, -5, 16, 33, 42, 103, 344]
+
+let firstItem = arr.shift();//removes first item
+console.log(firstItem, arr);//22, [-5, 16, 33, 42, 103, 344]
+
+let reversed = arr.reverse();//creates a new array in reverse order 
+console.log(reversed);//[344, 103, 42, 33, 16, -5]
+console.log(arr.join('-'));//"-5-103-16-33-344-42" joins array with provided separator
