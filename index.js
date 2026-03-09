1af93033d38922837a9e7b6f6e7e7706db01b578
AcronyxElectronator <acronyxelectronator@gmail.com>
2026-03-09 18:39:06 -0400

    Task 5.2

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function printDate(){
 setInterval(printDate, 1000);
 */
 
-//task 5.1 arrays
+/*task 5.1 arrays
 let arr = [ -5, 16, 33, 42, 103, 344];
 
 console.log(arr.includes(-5));//true
@@ -107,3 +107,40 @@ console.log(firstItem, arr);//22, [-5, 16, 33, 42, 103, 344]
 let reversed = arr.reverse();//creates a new array in reverse order 
 console.log(reversed);//[344, 103, 42, 33, 16, -5]
 console.log(arr.join('-'));//"-5-103-16-33-344-42" joins array with provided separator
+*/
+
+//task 5.2
+let arr = [12, 33, 4, 5, -4, 8, 19, 25];
+                
+//map() creates a new array from the elements of one without changing the old one
+function double(num){
+  return num * 2;
+}
+
+let doubledArr = arr.map(double); 
+console.log(doubledArr);
+
+function isOdd(num){
+  return num%2 !== 0; 
+}
+//Filter takes a test condition and returns only the element which 
+//make the condition true
+let odds = arr.filter(isOdd);
+console.log(odds);
+
+//Returns true or false if any of the elements of the array 
+//meets a specified condition
+function has5Factor(ele){
+  return ele % 5 === 0;
+}
+
+let hasFiveFactor = arr.some(has5Factor);
+console.log(hasFiveFactor);
+
+function intCompare(a, b){
+ return a - b;
+}
+
+//sort function must return either 0, +ve, -ve
+let ascending = arr.sort(intCompare)
+console.log(ascending);
